58dfab55de4890825d9ee12bf3ba5c7fcf230aa5
Pranav Bodawala <b.pranav.k@gmail.com>
2021-10-27 22:52:57 -0600

    done

---
+++ app.js
@@ -0,0 +1,24 @@
+import express, { response } from "express";
+
+const app = express();
+const PORT = 8000;
+const data = { rainbows: "waterfall" };
+
+const logger = (request, response, next) => {
+  console.log(`Got Request: ${JSON.stringify(request.url)}`);
+  next();
+};
+app.use(logger);
+
+app.get("/", logger, (request, response) => {
+  console.log("Hello World");
+  response.send("Hello World");
+});
+
+app.get("/api/data", (request, response) => {
+  response.send(data);
+});
+
+app.listen(PORT, () => {
+  console.log(`Starting Server on http://localhost:${PORT}`);
+});
