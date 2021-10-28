ca0fe6c7faaf5eb96e464c7b1b6959fc098de4b7
madeeha <maadeeeehaa@gmail.com>
2021-10-27 23:20:17 -0600

    :star: add data

--- app.js
+++ app.js
@@ -1,8 +1,47 @@
-import express, { response } from "express";
+import express, { response } from 'express';
 
 const app = express();
 const PORT = 8000;
-const data = { rainbows: "waterfall" };
+const data = {
+  labels: [
+    'October',
+    'November',
+    'December',
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+  ],
+  datasets: [
+    {
+      label: 'British Columbia ',
+      backgroundColor: '#f87979',
+      data: [
+        { x: '2021-10-01', y: 5 },
+        { x: '2021-11-01', y: 8 },
+        { x: '2021-12-01', y: 10 },
+      ],
+      borderColor: '#36495d',
+      borderWidth: 1,
+    },
+    {
+      label: 'Edmonton',
+      backgroundColor: '#f8449',
+      data: [
+        { x: '2021-10-01', y: 9 },
+        { x: '2021-11-01', y: 5 },
+        { x: '2021-12-01', y: 1 },
+      ],
+      borderColor: '#36495d',
+      borderWidth: 1,
+    },
+  ],
+};
 
 const logger = (request, response, next) => {
   console.log(`Got Request: ${JSON.stringify(request.url)}`);
@@ -10,12 +49,12 @@ const logger = (request, response, next) => {
 };
 app.use(logger);
 
-app.get("/", logger, (request, response) => {
-  console.log("Hello World");
-  response.send("Hello World");
+app.get('/', logger, (request, response) => {
+  console.log('Hello World');
+  response.send('Hello World');
 });
 
-app.get("/api/data", (request, response) => {
+app.get('/api/data', (request, response) => {
   response.send(data);
 });
 
